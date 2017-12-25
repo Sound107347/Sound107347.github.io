a32e79806e22bd518ec08254b60dd0722b7e8892
Sound107347 <33649250+Sound107347@users.noreply.github.com>
2017-12-25 11:55:58 +0300

    Add files via upload

---
+++ news/columnGrid.js
@@ -0,0 +1,19 @@
+function renderGrid(){
+	var blocks = document.getElementById("gridСontainer").children;
+	var pad = 10, cols = 3, newleft, newtop;
+	for(var i = 1; i < blocks.length; i++){
+		if (i % cols == 0) {
+			newtop = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight) + pad;
+			blocks[i].style.top = newtop+"px";
+			} else {
+				if(blocks[i-cols]){
+				newtop = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight) + pad;
+				blocks[i].style.top = newtop+"px";
+			}
+			newleft = (blocks[i-1].offsetLeft + blocks[i-1].offsetWidth) + pad;
+			blocks[i].style.left = newleft+"px";
+			}
+		}
+}
+window.addEventListener("load", renderGrid, false);
+window.addEventListener("resize", renderGrid, false);
