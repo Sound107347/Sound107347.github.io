1d06884b3791c26b9d5e8a4a7c2e33f0f7e18655
Bringerrr <Kruchkovevg21@gmail.com>
2018-03-04 13:04:24 +0200

    New version

--- README.md
+++
@@ -1,1 +0,0 @@
-# Sound107347.github.io
--- channels.js
+++ channels.js
@@ -1,14 +1,14 @@
-	var channel_max = 10;										// number of channels
+	var channel_max = 10;							
 	audiochannels = new Array();
-	for (a=0;a<channel_max;a++) {									// prepare the channels
+	for (a=0;a<channel_max;a++) {			
 		audiochannels[a] = new Array();
-		audiochannels[a]['channel'] = new Audio();						// create a new audio object
-		audiochannels[a]['finished'] = -1;							// expected end time for this channel
+		audiochannels[a]['channel'] = new Audio(); 
+		audiochannels[a]['finished'] = -1;		
 	}
-	function playAudio(s) {
+	function playCitation(s) {
 		for (a=0;a<audiochannels.length;a++) {
 			thistime = new Date();
-			if (audiochannels[a]['finished'] < thistime.getTime()) {			// is this channel finished?
+			if (audiochannels[a]['finished'] < thistime.getTime()) {			
 				audiochannels[a]['finished'] = thistime.getTime() + document.getElementById(s).duration*1000;
 				audiochannels[a]['channel'].src = document.getElementById(s).src;
 				audiochannels[a]['channel'].load();
